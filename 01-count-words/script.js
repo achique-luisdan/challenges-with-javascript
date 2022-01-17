48aa2edd0dfef3558baeea615a868ef27597c958
Achique Luis Daniel <46081094+achique-luisdan@users.noreply.github.com>
2022-01-17 09:00:15 -0400

    Compare strings converted all to lowercase

--- 01-count-words/script.js
+++ 01-count-words/script.js
@@ -1,6 +1,6 @@
 function countWord(phrase = '', word = '') {
-  const words = phrase.split(' ');
-  return words.filter(elemet => elemet == word).length;
+  const words = phrase.toLowerCase().split(' ');
+  return words.filter(elemet => elemet == word.toLowerCase()).length;
 }
 
 function setCounterWord(value = 0) {
@@ -11,12 +11,10 @@ function handlerCountWord() {
   const $phrase = document.getElementById('phrase');
   const $word = document.getElementById('word');
   const callCountWord = function () {
-      setCounterWord(countWord($phrase.value, $word.value));
+    setCounterWord(countWord($phrase.value, $word.value));
   };
   $phrase.addEventListener('change', callCountWord);
   $word.addEventListener('change', callCountWord);
 }
 
 handlerCountWord();
-
-
